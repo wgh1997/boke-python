ef6d0d964dd36dc1c17955b4cc5b058161fee213
wgh1997 <875193607@qq.com>
2022-11-06 00:18:49 +0800

    fix:

--- boke/es.py
+++ boke/es.py
@@ -1,19 +1,13 @@
-from .local import (
-    ES_NAME,
-    ES_PASSWOR,
-)
 from elasticsearch import Elasticsearch
 #查看证书位置
 import ssl
 ##忽视证书
 context = ssl._create_unverified_context()
-name = ES_NAME
-password = ES_PASSWOR
 ES =["42.192.148.228:9200"]
 # 创建elasticsearch客户端
 es = Elasticsearch(
     ES,
-    http_auth=(name,password),##账号密码
+    # http_auth=('elastic','yw181126'),##账号密码
     scheme="http",
     ssl_context=context,
 )
--- boke/local.py
+++
@@ -1,4 +0,0 @@
-from .settings import *
-
-ES_NAME = "elastic"
-ES_PASSWOR = 'yw181126'

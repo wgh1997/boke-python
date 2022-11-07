96e19dd65294cf30399b556478a0b326600f7120
wgh1997 <875193607@qq.com>
2022-11-07 16:15:34 +0800

    fix: 优化es 连接

--- boke/es.py
+++ boke/es.py
@@ -1,13 +1,20 @@
+import os
+from .local import (
+    ES_NAME,
+    ES_PASSWOR,
+)
 from elasticsearch import Elasticsearch
 #查看证书位置
 import ssl
 ##忽视证书
+
 context = ssl._create_unverified_context()
 ES =["42.192.148.228:9200"]
+print(ES_NAME,ES_PASSWOR)
 # 创建elasticsearch客户端
 es = Elasticsearch(
     ES,
-    http_auth=('elastic','yw181126'),##账号密码
+    http_auth=(ES_NAME,ES_PASSWOR),##账号密码
     scheme="http",
     ssl_context=context,
 )
